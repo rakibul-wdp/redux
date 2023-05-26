a95f0a1329a1db8a75d957b9ccd3b4ddf9dd5689
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-26 08:16:48 +0600

    create another counter with different state

--- Redux-Fundamentals/what-is-state-management/script.js
+++ Redux-Fundamentals/what-is-state-management/script.js
@@ -3,8 +3,13 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+const counterEl2 = document.getElementById("counter2");
+const incrementEl2 = document.getElementById("increment2");
+const decrementEl2 = document.getElementById("decrement2");
+
 // initial state
 let count = 0;
+let count2 = 0;
 
 // event listeners
 incrementEl.addEventListener("click", () => {
@@ -15,4 +20,14 @@ incrementEl.addEventListener("click", () => {
 decrementEl.addEventListener("click", () => {
   count--;
   counterEl.innerText = count;
+});
+
+incrementEl2.addEventListener("click", () => {
+  count2++;
+  counterEl2.innerText = count2;
+});
+
+decrementEl2.addEventListener("click", () => {
+  count2--;
+  counterEl2.innerText = count2;
 });
