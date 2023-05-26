de49dd21a9c965758630932c9dce67a301463b6a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-26 08:49:09 +0600

    counter app convert boilarplate by react

---
+++ Redux-Fundamentals/1.2-reactive-ui-&-reusability/counter/src/App.jsx
@@ -0,0 +1,20 @@
+import Counter from "./components/Counter"
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      {/* <!-- header --> */}
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      {/* <!-- counters --> */}
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        <Counter />
+      </div>
+    </div>
+  )
+}
+
+export default App
