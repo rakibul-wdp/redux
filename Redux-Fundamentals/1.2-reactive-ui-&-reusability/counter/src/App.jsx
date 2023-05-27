7d7a08549476999ca97025f27a7753d67ee86c90
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-27 11:52:56 +0600

    manage state in complex way

--- Redux-Fundamentals/1.2-reactive-ui-&-reusability/counter/src/App.jsx
+++ Redux-Fundamentals/1.2-reactive-ui-&-reusability/counter/src/App.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react"
 import Counter from "./components/Counter"
 import Stats from "./components/Stats"
 
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  }
+]
+
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  }
+
+  const increment = (id) => {
+    const updatedCounter = state.map(c => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        }
+      }
+      return { ...c };
+    })
+
+    setState(updatedCounter);
+  }
+
+  const decrement = (id) => {
+    const updatedCounter = state.map(c => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count - 1,
+        }
+      }
+      return { ...c };
+    })
+
+    setState(updatedCounter);
+  }
 
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -12,9 +57,12 @@ function App() {
 
       {/* <!-- counters --> */}
       <div className="max-w-md mx-auto mt-10 space-y-5">
-        <Counter />
-        <Counter />
-        <Stats count={2} />
+        {
+          state.map((count) => (
+            <Counter key={count.id} id={count.id} count={count.count} increment={increment} decrement={decrement} />
+          ))
+        }
+        <Stats count={totalCount()} />
       </div>
     </div>
   )
--- Redux-Fundamentals/1.2-reactive-ui-&-reusability/counter/src/components/Counter.jsx
+++ Redux-Fundamentals/1.2-reactive-ui-&-reusability/counter/src/components/Counter.jsx
@@ -1,36 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Count from './Count';
+import Button from './Button';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  }
-
+const Counter = ({ id, count, increment, decrement }) => {
   return (
     <div
       className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
     >
-      <div className="text-2xl font-semibold" id="counter">{count}</div>
+      <Count count={count} />
       <div className="flex space-x-3">
-        <button
-          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-          id="increment"
-          onClick={increment}
-        >
-          Increment
-        </button>
-        <button
-          className="bg-red-400 text-white px-3 py-2 rounded shadow"
-          id="decrement"
-          onClick={decrement}
-        >
-          Decrement
-        </button>
+        <Button handler={() => increment(id)} >Increment</Button>
+        <Button handler={() => decrement(id)} >Decrement</Button>
       </div>
     </div>
   )
