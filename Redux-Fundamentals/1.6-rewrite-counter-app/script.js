6f29d89df680f63d066fc7e07a77f2f405a5b8b0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-27 12:24:46 +0600

    create reducer function

---
+++ Redux-Fundamentals/1.6-rewrite-counter-app/script.js
@@ -0,0 +1,21 @@
+// initial state
+const initialState = {
+  value: 0,
+}
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    }
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    }
+  } else {
+    return state;
+  }
+}
