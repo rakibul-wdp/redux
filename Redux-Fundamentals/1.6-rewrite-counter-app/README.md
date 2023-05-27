592db8e27429e1bfb2b453ef113bf6ed31ea7c6b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-27 12:43:26 +0600

    use apyload to variablly change state

--- Redux-Fundamentals/1.6-rewrite-counter-app/README.md
+++ Redux-Fundamentals/1.6-rewrite-counter-app/README.md
@@ -1,2 +1,5 @@
 # Rewrite the Counter application
 ### with vanilla Redux
+
+# Action Payloads & Creators
+### Why we need Action Creators
--- Redux-Fundamentals/1.6-rewrite-counter-app/script.js
+++ Redux-Fundamentals/1.6-rewrite-counter-app/script.js
@@ -13,12 +13,12 @@ function counterReducer(state = initialState, action) {
   if (action.type === "increment") {
     return {
       ...state,
-      value: state.value + 1,
+      value: state.value + action.payload,
     }
   } else if (action.type === "decrement") {
     return {
       ...state,
-      value: state.value - 1,
+      value: state.value - action.payload,
     }
   } else {
     return state;
@@ -41,12 +41,14 @@ store.subscribe(render);
 // button click listeners
 incrementEl.addEventListener("click", () => {
   store.dispatch({
-    type: "increment",
+    type: "increment", // mandatory
+    payload: 5,
   });
 })
 
 decrementEl.addEventListener("click", () => {
   store.dispatch({
-    type: "decrement",
+    type: "decrement", // mandatory
+    payload: 2,
   });
 })
