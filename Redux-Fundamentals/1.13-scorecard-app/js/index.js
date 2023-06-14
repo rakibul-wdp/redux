8eda5f6268ab77282d704cc18f3d3607bb87e754
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-14 18:37:40 +0600

    create action creators

--- Redux-Fundamentals/1.13-scorecard-app/js/index.js
+++ Redux-Fundamentals/1.13-scorecard-app/js/index.js
@@ -3,3 +3,32 @@ const INCREMENT = "increment";
 const DECREMENT = "decrement";
 const ADD = "add";
 const RESET = "reset";
+
+// action creators
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value
+  }
+}
+
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value
+  }
+}
+
+const add = (value) => {
+  return {
+    type: ADD,
+    payload: value
+  }
+}
+
+const reset = (value) => {
+  return {
+    type: RESET,
+    payload: value
+  }
+}
