92f48214f6c4abb717f859ba2668ba8c94d3acbc
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-19 16:03:42 +0600

    crate another dynamic counter feature

--- react-redux/2.1-setup-react-redux/src/redux/store.js
+++ react-redux/2.1-setup-react-redux/src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
+import dynamicCounterReducer from "./dynamicCounter/dynamicCounterReducer";
 
 const store = createStore(counterReducer);
 
