922de0ce4bf9f632a62352d25d40e9e428595b65
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-11 09:02:31 +0600

    create reducer and handle form

--- ph/module-2_problem-we-solving-using-redux/src/components/LongForm.jsx
+++ ph/module-2_problem-we-solving-using-redux/src/components/LongForm.jsx
@@ -9,15 +9,32 @@ const LongForm = () => {
     education: "",
     quantity: 0,
     feedback: "",
-    term: true,
+    term: false,
   };
 
-  const reducer = (state, action) => {};
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INPUT":
+        return {
+          ...state,
+          [action.payload.name]: action.payload.value,
+        };
+      case "TOGGLE":
+        return {
+          ...state,
+          term: !state.term,
+        };
+      default:
+        return state;
+    }
+  };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    console.log(state);
   };
 
   return (
@@ -30,37 +47,100 @@ const LongForm = () => {
           <label className="mb-2" htmlFor="firstName">
             First Name
           </label>
-          <input type="text" name="firstName" id="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            onBlur={(e) =>
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              })
+            }
+          />
         </div>
         <div className="flex flex-col w-full max-w-xs">
           <label className="mb-2" htmlFor="lastName">
             Last Name
           </label>
-          <input type="text" name="lastName" id="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            onBlur={(e) =>
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              })
+            }
+          />
         </div>
         <div className="flex flex-col w-full max-w-xs">
           <label className="mb-2" htmlFor="email">
             Email
           </label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onBlur={(e) =>
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              })
+            }
+          />
         </div>
         <div className="flex flex-col w-full max-w-xs">
           <h1 className="mb-3">Gender</h1>
           <div className="flex gap-3">
             <div>
-              <input type="radio" id="male" name="gender" value="male" />
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                onClick={(e) =>
+                  dispatch({
+                    type: "INPUT",
+                    payload: { name: e.target.name, value: e.target.value },
+                  })
+                }
+              />
               <label className="ml-2 text-lg" htmlFor="male">
                 Male
               </label>
             </div>
             <div>
-              <input type="radio" id="female" name="gender" value="female" />
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                onClick={(e) =>
+                  dispatch({
+                    type: "INPUT",
+                    payload: { name: e.target.name, value: e.target.value },
+                  })
+                }
+              />
               <label className="ml-2 text-lg" htmlFor="female">
                 Female
               </label>
             </div>
             <div>
-              <input type="radio" id="other" name="gender" value="other" />
+              <input
+                type="radio"
+                id="other"
+                name="gender"
+                value="other"
+                onClick={(e) =>
+                  dispatch({
+                    type: "INPUT",
+                    payload: { name: e.target.name, value: e.target.value },
+                  })
+                }
+              />
               <label className="ml-2 text-lg" htmlFor="other">
                 Other
               </label>
@@ -71,7 +151,16 @@ const LongForm = () => {
           <label className="mb-3" htmlFor="education">
             Education
           </label>
-          <select name="education" id="education">
+          <select
+            name="education"
+            id="education"
+            onChange={(e) =>
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              })
+            }
+          >
             <option value="SSC">SSC</option>
             <option value="HSC">HSC</option>
             <option value="underGrad">Under Graduate</option>
@@ -96,17 +185,35 @@ const LongForm = () => {
           <label className="mb-3" htmlFor="feedback">
             Feedback
           </label>
-          <textarea name="feedback" id="feedback" cols="30" rows="4"></textarea>
+          <textarea
+            name="feedback"
+            id="feedback"
+            cols="30"
+            rows="4"
+            onBlur={(e) =>
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              })
+            }
+          ></textarea>
         </div>
 
         <div className="flex justify-between items-center w-full">
           <div className="flex  w-full max-w-xs">
-            <input className="mr-3" type="checkbox" name="term" id="terms" />
+            <input
+              className="mr-3"
+              type="checkbox"
+              name="term"
+              id="terms"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
             <label htmlFor="terms">I agree to terms and conditions</label>
           </div>
           <button
             className=" px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500"
             type="submit"
+            disabled={!state.term}
           >
             Submit
           </button>
