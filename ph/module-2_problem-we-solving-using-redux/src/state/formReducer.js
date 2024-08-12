a2d7b21a241ebaa3403ea2de32f46d26ec94ad84
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-12 10:28:09 +0600

    also separate action types

--- ph/module-2_problem-we-solving-using-redux/src/state/formReducer.js
+++ ph/module-2_problem-we-solving-using-redux/src/state/formReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from "./actionTypes";
+
 export const initialState = {
   firstName: "",
   lastName: "",
@@ -11,22 +13,22 @@ export const initialState = {
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case "INPUT":
+    case actionTypes.INPUT:
       return {
         ...state,
         [action.payload.name]: action.payload.value,
       };
-    case "TOGGLE":
+    case actionTypes.TOGGLE:
       return {
         ...state,
         term: !state.term,
       };
-    case "INCREMENT":
+    case actionTypes.INCREMENT:
       return {
         ...state,
         quantity: state.quantity + 1,
       };
-    case "DECREMENT":
+    case actionTypes.DECREMENT:
       return {
         ...state,
         quantity: state.quantity - 1,
