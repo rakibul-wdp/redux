d97736502f194e8f77a8e3e82ae00636e590150a
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-15 08:59:32 +0600

    fix unique key issue

--- ph/module-2_moon-tech/src/pages/Home.js
+++ ph/module-2_moon-tech/src/pages/Home.js
@@ -4,14 +4,23 @@ import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
   const {
-    state: { products },
+    state: { products, loading, error },
   } = useProducts();
+
+  let content;
+
+  if (loading) content = <p>Loading...!!!</p>;
+  if (error) content = <p>Something went wrong!</p>;
+  if (!loading && !error && products.length === 0)
+    content = <p>Nothing to show, product list is empty!</p>;
+  if (!loading && !error && products.length)
+    content = products.map((product, index) => (
+      <ProductCard key={index} product={product} />
+    ));
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {products.map((product) => (
-        <ProductCard product={product} />
-      ))}
+      {content}
     </div>
   );
 };
