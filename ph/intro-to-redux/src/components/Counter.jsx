0d58dc1e3f4d3894ed88b9758db972309ad17e45
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-11 15:23:43 +0600

    dispatch action in counter component

--- ph/intro-to-redux/src/components/Counter.jsx
+++ ph/intro-to-redux/src/components/Counter.jsx
@@ -1,17 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
   const { count } = useSelector((state) => state.counter);
-
-  console.log(count);
+  const dispatch = useDispatch();
 
   return (
     <div>
-      <button>Increment</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
       <div>
         <h1>{count}</h1>
       </div>
-      <button>Decrement</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
