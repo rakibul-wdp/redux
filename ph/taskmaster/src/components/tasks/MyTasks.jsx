a95f3c0b1de4bde69fa5ee77cb0858096f2b6468
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-21 12:22:48 +0600

    create task details modal and make it functional

--- ph/taskmaster/src/components/tasks/MyTasks.jsx
+++ ph/taskmaster/src/components/tasks/MyTasks.jsx
@@ -2,13 +2,21 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { userTasks } from "../../redux/features/tasks/taskSlice";
+import { updateStatus, userTasks } from "../../redux/features/tasks/taskSlice";
+import TaskDetailsModal from "./TaskDetailsModal";
 
 const MyTasks = () => {
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.userSlice);
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId, setTaskId] = useState(0);
+
+  const handleModal = (id) => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  };
 
   const dispatch = useDispatch();
 
@@ -18,6 +26,7 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} id={taskId} />
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userSpecificTasks?.map((item) => (
@@ -27,10 +36,20 @@ const MyTasks = () => {
           >
             <h1>{item.title}</h1>
             <div className="flex gap-3">
-              <button className="grid place-content-center" title="Details">
+              <button
+                onClick={() => handleModal(item.id)}
+                className="grid place-content-center"
+                title="Details"
+              >
                 <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
               </button>
-              <button className="grid place-content-center" title="Done">
+              <button
+                onClick={() =>
+                  dispatch(updateStatus({ id: item.id, status: "done" }))
+                }
+                className="grid place-content-center"
+                title="Done"
+              >
                 <CheckIcon className="w-5 h-5 text-primary" />
               </button>
             </div>
