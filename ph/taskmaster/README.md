3bf08699cf8bb52b91efd0f9e1c286d865232387
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-21 11:34:46 +0600

    create user task slice

--- ph/taskmaster/README.md
+++ ph/taskmaster/README.md
@@ -1,1 +1,5 @@
-Tasksmaster Server => https://github.com/mir-hussain/taskmaster-server
+### Task
+
+- click tick icon to done the task
+- click details icon to open modal
+- show my task related to the user
--- ph/taskmaster/src/redux/features/tasks/taskSlice.js
+++ ph/taskmaster/src/redux/features/tasks/taskSlice.js
@@ -13,6 +13,7 @@ const initialState = {
       priority: "high",
     },
   ],
+  userTasks: [],
 };
 
 const tasksSlice = createSlice({
@@ -32,11 +33,16 @@ const tasksSlice = createSlice({
       }
     },
     removeTask: (state, { payload }) => {
-      state.tasks.filter((item) => item.id !== payload);
+      state.tasks = state.tasks.filter((item) => item.id !== payload);
     },
     updateStatus: (state, { payload }) => {
       const target = state.tasks.find((item) => item.id === payload.id);
       target.status = payload.status;
+    },
+    userTasks: (state, { payload }) => {
+      state.userTasks = state.tasks.filter(
+        (item) => item.assignedTo === payload
+      );
     },
   },
 });
