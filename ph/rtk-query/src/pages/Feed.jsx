05c4e53ba6093e3dd77cc30ce8a328fe744ea06c
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-22 08:37:43 +0600

    create pots card component

--- ph/rtk-query/src/pages/Feed.jsx
+++ ph/rtk-query/src/pages/Feed.jsx
@@ -1,4 +1,8 @@
+import { useGetPostsQuery } from "../redux/features/api/baseApi";
+
 const Feed = () => {
+  const { data: posts, isLoading, isError, error } = useGetPostsQuery();
+
   return <div>Feed</div>;
 };
 
