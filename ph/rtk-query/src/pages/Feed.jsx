a5465f8938b8ca12fce78828eb080e7f0bf1aad2
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-22 09:21:08 +0600

    show post data

--- ph/rtk-query/src/pages/Feed.jsx
+++ ph/rtk-query/src/pages/Feed.jsx
@@ -1,9 +1,27 @@
+import PostCard from "../components/layout/PostCard";
 import { useGetPostsQuery } from "../redux/features/api/baseApi";
 
 const Feed = () => {
-  const { data: posts, isLoading, isError, error } = useGetPostsQuery();
+  const { data: posts, isLoading, isError } = useGetPostsQuery();
 
-  return <div>Feed</div>;
+  if (isLoading) {
+    return <p className="text-9xl text-zinc-300">Loading...!!!</p>;
+  }
+
+  if (!isLoading && isError) {
+    return <p className="text-9xl text-zinc-300">Something wrong</p>;
+  }
+
+  return (
+    <div>
+      <h1>Feed</h1>
+      <div className="flex flex-col gap-3">
+        {posts?.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
